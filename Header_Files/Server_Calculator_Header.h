26bd18b139edf8b5bc6b5f1a3ee166ae61a04e39
Vaibhav T Gaikwad <103184302+VaibhavGaikwad03@users.noreply.github.com>
2023-03-30 17:46:42 +0530

    Add files via upload

---
+++ Header_Files/Server_Calculator_Header.h
@@ -0,0 +1,24 @@
+#ifndef CALCULATOR_HEADER_H
+#define CALCULATOR_HEADER_H
+
+#include "Server_Stack_Header.h"
+#define MAX 50
+
+class ScientificCalculator
+{
+    Stack *m_pStack;
+    const char *m_cpInfix;
+    char m_pPostfix[MAX];
+
+public:
+    ScientificCalculator(const char *pInfix = NULL);
+    ~ScientificCalculator();
+    long evaluate(const char *cpInfix = NULL);
+
+private:
+    void infix_to_postfix();
+    bool is_space(char chSymbol);
+    int is_operator(char chSymbol);
+};
+
+#endif //CALCULATOR_HEADER_H
